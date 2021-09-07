9d24d2203fa1006368a629b858623391e7af56ff
Eric Almendras <ericalmendras@hotmail.com>
2021-09-07 15:49:51 +0200

    Implementacion de la clase Producto

--- src/tierramedia/Producto.java
+++ src/tierramedia/Producto.java
@@ -1,5 +1,39 @@
 package tierramedia;
 
-public abstract class Producto implements Comparable<Producto>{
-	//al usar la interfaz comparable hay que sobreescribir el metodo compareTo
+public abstract class Producto{
+	protected TipoAtraccion tipo;
+	protected String nombre;
+	protected Integer precio;
+	protected Double tiempo;
+	
+	public Producto(TipoAtraccion tipo, String nombre, Integer precio, Double tiempo) {
+		this.tipo = tipo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.tiempo = tiempo;
+	}
+	
+	public TipoAtraccion getTipo() {
+		return tipo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+
+	public Double getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(Double tiempo) {
+		this.tiempo = tiempo;
+	}	
 }
