a846384de2d6c107aa03cb596815599ef72ca315
Eric Almendras <ericalmendras@hotmail.com>
2021-09-07 15:49:27 +0200

    Implementacion de la clase Atraccion

--- src/tierramedia/Atraccion.java
+++ src/tierramedia/Atraccion.java
@@ -1,5 +1,28 @@
 package tierramedia;
 
-public class Atraccion {
+public class Atraccion extends Producto{
+	private Integer cupo;
+	
+	public Atraccion(String nombre, Integer costo, Double duracion, int cupo, TipoAtraccion tipo) {
+		super(tipo,nombre,costo,duracion);
+		this.cupo = cupo;
+	}
 
+	public Integer getCupo() {
+		return cupo;
+	}
+
+	public void setCupo(Integer cupo) {
+		this.cupo = cupo;
+	}
+	
+	public Boolean hayCupo() {
+		return cupo > 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Atraccion ( " + super.getNombre() + ", Precio : " + super.getPrecio() + ", Duracion : " + super.getTiempo() + ", Cupos Restantes: " + cupo
+				+ ", Tipo : " + super.getTipo() + ") ";
+	}	
 }
